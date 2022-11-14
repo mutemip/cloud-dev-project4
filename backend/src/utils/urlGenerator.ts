51c44e11c41cf2936e5c9b44fb05bf7fce821c89
mutemip <paulmutemi2030@gmail.com>
2022-11-14 15:00:56 +0300

    changed app-name && bucket name

--- backend/src/utils/urlGenerator.ts
+++ backend/src/utils/urlGenerator.ts
@@ -6,13 +6,13 @@ export const generateUrl = async (id: string, imageType: string) => {
     const s3 = new enableTracing.S3({ region: 'us-east-1' });
     const tod = `${id}`;
     const url = s3.getSignedUrl('putObject', {
-        Bucket: 'ribiroattachementseou7xk8j16a4',
+        Bucket: 'mutemipbucketnew1234retwr',
         Key: tod,
         Expires: 3600,
         ContentType: imageType,
         ACL: 'public-read',
     });
-    let fileUrl: string = "https://ribiroattachementseou7xk8j16a4.s3.amazonaws.com/" + id;
+    let fileUrl: string = "https://mutemipbucketnew1234retwr.s3.amazonaws.com/" + id;
     let item = await getSingleTodo(id);
     return { item, fileUrl, url };
 }
